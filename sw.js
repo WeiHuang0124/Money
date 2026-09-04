a2cafb65e9035c26ba213a48c79322c8f256c213
WeiHuang <huang.870124@gmail.com>
2026-09-04 11:21:35 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='okr-hub-v1';
-const ASSETS=['./','./index.html','./quest.html','./budget.html','./center.html','./roadmap.html','./plan.html','./finance.html',
+const ASSETS=['./','./index.html','./quest.html','./budget.html','./center.html','./roadmap.html','./plan.html','./finance.html','./rescue.html',
   './manifest.json','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
